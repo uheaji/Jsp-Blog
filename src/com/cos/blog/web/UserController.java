c38adeb30d9f6f18cbbca3c2d39d37843e6fb337
unknown <you@example.com>
2020-12-31 15:33:31 +0900

    2. Jsp블로그 프로젝트구조 잡기(1/2)

---
+++ src/com/cos/blog/web/UserController.java
@@ -0,0 +1,32 @@
+package com.cos.blog.web;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+// http://localhost:8000/blog/user
+@WebServlet("/user")
+public class UserController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    
+    public UserController() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doProcess(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doProcess(request, response);
+	}
+	
+	protected void doProcess(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+}
