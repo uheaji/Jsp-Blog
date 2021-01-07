fdc612d88534fd887b0700096283d621c2722895
yoolbk <you@example.com>
2021-01-07 12:56:47 +0900

    10. 글쓰기 기능 구현하기

--- src/com/cos/blog/service/BoardService.java
+++ src/com/cos/blog/service/BoardService.java
@@ -1,5 +1,17 @@
 package com.cos.blog.service;
 
-public class BoardService {
+import com.cos.blog.domain.board.BoardDao;
+import com.cos.blog.domain.board.dto.SaveReqDto;
 
+public class BoardService {
+	
+	private BoardDao boardDao;
+	
+	public BoardService() {
+		boardDao =  new BoardDao();
+	}
+	
+	public int 글쓰기(SaveReqDto dto) {
+		return boardDao.save(dto);
+	}
 }
