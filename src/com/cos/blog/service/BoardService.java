015af8774f35b5002752aceeb65c73155586d6dd
yoolbk <you@example.com>
2021-01-08 11:43:20 +0900

    13. 글 목록보기(2/2), 페이징완료

--- src/com/cos/blog/service/BoardService.java
+++ src/com/cos/blog/service/BoardService.java
@@ -18,8 +18,8 @@ public class BoardService {
 		return boardDao.save(dto);
 	}
 	
-	public List<Board> 글목록보기(){
-		return boardDao.findAll();
+	public List<Board> 글목록보기(int page){
+		return boardDao.findAll(page);
 	}
 
 }
